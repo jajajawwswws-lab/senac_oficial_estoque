f02a3a4b70d2cbf8ab509cfd5d7a719f50b14c03
linuxlinux <jajajawwswws@gmail.com>
2026-03-01 09:28:13 -0500

    Remove arquivos compilados do TypeScript e atualiza .gitignore

--- api/backend.d.ts
+++
@@ -1,4 +0,0 @@
-import { IncomingMessage, ServerResponse } from "node:http";
-declare function ServerRequest(request: IncomingMessage, response: ServerResponse): Promise<void>;
-export default ServerRequest;
-//# sourceMappingURL=backend.d.ts.map
